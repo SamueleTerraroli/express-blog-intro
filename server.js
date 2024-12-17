25a79b20cef08b58672f5c6429b4536912f2521b
SamueleTerraroli <terraroli99@gmail.com>
2024-12-17 15:49:09 +0100

    feat:add img root and fix server issues

--- server.js
+++ server.js
@@ -1,19 +1,37 @@
-const express = require ('express');
+const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
 const posts = require('./posts');
 
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
     res.send('server del mio blog');
 });
-app.listen(port, ()=>{
-    
-})
 
-app.get('/bacheca',(req , res)=>{
+app.get('/bacheca', (req, res) => {
     res.json({
-        id: posts.length,
+        count: posts.length,
         posts: posts
     });
-})
+});
+
+app.get('/img', (req, res) => {
+    const postTitle = req.query.title;
+    const post = posts.find(post => post.title === postTitle);
+    
+    if (post) {
+        const imgName = path.basename(post.img); // Usa la proprietà 'img'
+        const options = {
+            root: path.join(__dirname, 'img')
+        };
+
+        res.sendFile(imgName, options);
+    } else {
+        res.status(404).send('Immagine non trovata');
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server avviato sulla porta ${port}`);
+});

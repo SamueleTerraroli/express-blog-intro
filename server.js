e156890bd16b6a353908b4cae512640d4a0e1446
SamueleTerraroli <terraroli99@gmail.com>
2024-12-17 14:47:43 +0100

    fix: server listen

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require ('express');
 const app = express();
 const port = 3000;
 
+const posts = require('./posts');
+
 app.get('/', (req, res) =>{
     res.send('server del mio blog');
 });
+app.listen(port, ()=>{
+    
+})
+
